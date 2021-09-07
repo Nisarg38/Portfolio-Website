b857287c34cad925888efa1b725913e712002ab3
Nisarg Patel <nisarg38@gmail.com>
2021-09-07 16:49:35 +0530

    Loader-Animation-Update

--- src/App.js
+++ src/App.js
@@ -2,12 +2,30 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import { HashRouter as Router } from "react-router-dom";
 import Home from "./pages";
+import Loading from "./components/Priview/Loading";
 
 function App() {
+  const [loading, setloading] = useState(false);
+
+  useEffect(() => {
+    setloading(true);
+    setTimeout(() => {
+      setloading(false);
+    }, 5000);
+  }, []);
+
   return (
     <Router>
       <>
-        <Home />
+        {loading ? (
+          <>
+            <Loading />{" "}
+          </>
+        ) : (
+          <>
+            <Home />
+          </>
+        )}
       </>
     </Router>
   );
