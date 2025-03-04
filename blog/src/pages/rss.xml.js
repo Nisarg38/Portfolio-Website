a428edc4285b90c9cae115c742da3bc9da7a4f8d
Nisarg Patel <63173283+Nisarg38@users.noreply.github.com>
2025-03-03 19:47:53 -0500

    Bug Fix

--- blog/src/pages/rss.xml.js
+++ blog/src/pages/rss.xml.js
@@ -10,7 +10,7 @@ export async function GET(context) {
 		site: context.site,
 		items: posts.map((post) => ({
 			...post.data,
-			link: `/Portfolio-Website/blog/blogs/${post.id}/`,
+			link: `/Portfolio-Website/blog/blogs/${post.id}`,
 		})),
 	});
 }
